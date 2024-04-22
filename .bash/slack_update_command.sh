5724facbb320ce348cdd065470fc70fa19ec0ec6
jean-brice <jbgachot@users.noreply.github.com>
2024-04-22 19:46:36 +0200

    Allow to define an url in the slack update command

--- .bash/slack_update_command.sh
+++ .bash/slack_update_command.sh
@@ -1,9 +1,14 @@
 echo -e "Updating the Slack App..."
 
 JSON_MANIFEST=$(jq --arg APP_ID ${args[--app-id]} '{"app_id": $APP_ID, "manifest": .}' ${args[--manifest]} | jq -c .)
-BANKER_ENDPOINT=$(cd packages/banker && serverless info --verbose | grep ServiceEndpoint | sed s/ServiceEndpoint\:\ //g)
+if [ -z ${args[--target]} ]; then
+    BANKER_ENDPOINT=$(cd packages/banker && serverless info --verbose | grep ServiceEndpoint | sed s/ServiceEndpoint\:\ //g)
+    REPORTS_ENDPOINT=$(cd packages/reports && serverless info --verbose | grep ServiceEndpoint | sed s/ServiceEndpoint\:\ //g)
+else
+    BANKER_ENDPOINT=${args[--target]}
+    REPORTS_ENDPOINT=${args[--target]}
+fi
 JSON_MANIFEST="${JSON_MANIFEST//<<BANKER_ENDPOINT>>/"$BANKER_ENDPOINT"}"   
-REPORTS_ENDPOINT=$(cd packages/reports && serverless info --verbose | grep ServiceEndpoint | sed s/ServiceEndpoint\:\ //g)
 JSON_MANIFEST="${JSON_MANIFEST//<<REPORTS_ENDPOINT>>/"$REPORTS_ENDPOINT"}"   
 
 curl -X POST -H "Content-Type: application/json; charset=utf-8" \
